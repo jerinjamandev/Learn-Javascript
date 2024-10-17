ee607ffdb051d045627b40d563dd7a173a7955e0
nasimajaman <nasimajaman12345@gmail.com>
2024-10-17 01:36:45 -0700

    typeof.js

---
+++ typeof.js
@@ -0,0 +1,18 @@
+//strinf type
+
+var name = "jerin jaman"
+console.log(name);
+
+var address = "gazipur borobari"
+console.log(address);
+
+
+//boolean
+
+var isRich = true;
+console.log(typeof isRich);
+
+//number
+
+var price = 999;
+console.log(typeof price);
